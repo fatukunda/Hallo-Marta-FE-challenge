1c24aa8b83fcd3454d994dc0e598d76bd496b005
Frank Atukunda <fatukunda@gmail.com>
2021-06-12 13:19:17 +0300

    Implement searching of stocks

--- src/components/stock-data/index.js
+++ src/components/stock-data/index.js
@@ -1,17 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
 import "./index.css";
 
 export default function StockData() {
+
+  const [date, setDate] = useState('');
+  const [stock, setStock] = useState(null);
+  const [isInitial, setIsInitial] = useState(true);
+
+  async function fetchStocks() {
+    const baseUrl = 'https://jsonmock.hackerrank.com/api/stocks'
+    try {
+      const response = await fetch(`${baseUrl}?date=${date}`);
+      const { data } = await response.json();
+      if (data.length) {
+        setIsInitial(false);
+        const firstStock = data[0];
+        setStock({
+          Open: firstStock.open,
+          Close: firstStock.close,
+          High: firstStock.high,
+          Low: firstStock.low
+        });
+      } else {
+        setStock(null);
+        setIsInitial(false);
+      }
+    } catch (error) {
+      console.log(error);// To be replaced with pop up error message
+    }
+  }
+
   return (
     <div className="layout-column align-items-center mt-50">
       <section className="layout-row align-items-center justify-content-center">
-        <input type="text" className="large" placeholder="5-January-2000" id="app-input" data-testid="app-input"/>
-        <button className="" id="submit-button" data-testid="submit-button">Search</button>
+        <input
+          type="text"
+          className="large"
+          placeholder="5-January-2000"
+          id="app-input"
+          data-testid="app-input"
+          onChange={(e) => setDate(e.target.value)}
+        />
+        <button
+          className=""
+          id="submit-button"
+          data-testid="submit-button"
+          onClick={fetchStocks}
+          >
+            Search
+          </button>
       </section>
-      <ul className="mt-50 slide-up-fade-in styled" id="stockData" data-testid="stock-data">
-        <li className="py-10"></li>
-      </ul>
-      <div className="mt-50 slide-up-fade-in" id="no-result" data-testid="no-result"></div>
+        {
+          !isInitial && stock ? (<ul className="mt-50 slide-up-fade-in styled" id="stockData" data-testid="stock-data">
+          {
+            Object.entries(stock).map(([key, value]) =>
+              <li className="py-10" key={key}>{`${key}: ${value}`}</li>)
+          }
+          </ul>):
+          !isInitial && !stock ?
+          <div className="mt-50 slide-up-fade-in" id="no-result" data-testid="no-result">
+          No Results Found
+        </div> : null
+        }
     </div>
   );
 }
